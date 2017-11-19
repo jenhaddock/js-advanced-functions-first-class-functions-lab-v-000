360976d47eb6efe58dd86e2a1ac4c1af320a4410
Jennifer Haddock <jentx123@hotmail.com>
2017-11-19 03:54:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
 // Code your solution in this file!
-const returnFirstTwoDrivers = function(array) { return array[0], array[1]}
+const returnFirstTwoDrivers = function(array) { return array.slice(1)};
