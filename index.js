e3985f6de3c0df084fa5c0b99ab5e09a14189fc4
Jennifer Haddock <jentx123@hotmail.com>
2017-11-19 04:00:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ const returnFirstTwoDrivers = function(array) { return array.slice(0,2)};
 const returnLastTwoDrivers = function(array) { return array.slice(-2)};
 
 const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function(int) {
+  return function (fare) {
+    return int * fare;
+  }
+}
