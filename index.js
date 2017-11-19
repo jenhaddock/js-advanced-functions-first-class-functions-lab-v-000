8c6a8aca06b003b09722dc2009e1db1f80a7082e
Jennifer Haddock <jentx123@hotmail.com>
2017-11-19 04:05:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,5 +16,5 @@ const fareDoubler = createFareMultiplier(2);
 const fareTripler = createFareMultiplier(3);
 
 const selectDifferentDrivers = function(array, f) {
-  f(array);
+  return f(array);
 }
